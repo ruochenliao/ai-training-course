97a0c1295ec17c17b003689974e99f5767b0a69a
zhangzhixiong <1923681655@qq.com>
2025-06-20 23:26:11 +0800

    Revert "完善智能客服功能"
    This reverts commit 631d37139506e7781c88242a7429fb55b7a66096.

--- IntelligentCustomerService/IntelligentCustomerService/app/services/__init__.py
+++ IntelligentCustomerService/IntelligentCustomerService/app/services/__init__.py
@@ -1,15 +1,3 @@
 """
 服务模块
 """
-
-from .voice_service import VoiceService
-from .image_generation_service import ImageGenerationService
-from .collaboration_service import CollaborationService
-from .analytics_service import AnalyticsService
-
-__all__ = [
-    'VoiceService',
-    'ImageGenerationService',
-    'CollaborationService',
-    'AnalyticsService'
-]
